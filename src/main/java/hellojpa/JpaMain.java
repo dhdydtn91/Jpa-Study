cdfa6282ca4bfb8b013164a98549c24a9f55c563
dhdydtn2019 <dhdydtn2019@gmail.com>
2020-05-09 14:22:50 +0900

    JPA 상속관계 매핑

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,44 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            //저장
+            Movie movie = new Movie();
+            movie.setDirector("aaaaa");
+            movie.setActor("bbbb");
+            movie.setName("바람과 함께 사라지다");
+            movie.setPrice(10000);
+
+            em.persist(movie);
+
+            em.flush();
+            em.clear();
+
+            Item item = em.find(Item.class , movie.getId());
+            System.out.println("findMovie = " + item);
+
+            tx.commit();
+        } catch(Exception e){
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+        emf.close();
+    }
+}

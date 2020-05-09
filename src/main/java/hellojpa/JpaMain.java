129e1bd625fa637a488299b6c8dc4f9f065de4e8
dhdydtn2019 <dhdydtn2019@gmail.com>
2020-05-09 15:26:55 +0900

    @MappedSuperClass 예제

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,10 +1,11 @@
 package hellojpa;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
-import java.util.List;
+        import javax.persistence.EntityManager;
+        import javax.persistence.EntityManagerFactory;
+        import javax.persistence.EntityTransaction;
+        import javax.persistence.Persistence;
+        import java.time.LocalDateTime;
+        import java.util.List;
 
 public class JpaMain {
 
@@ -18,21 +19,16 @@ public class JpaMain {
         tx.begin();
 
         try {
-            //저장
-            Movie movie = new Movie();
-            movie.setDirector("aaaaa");
-            movie.setActor("bbbb");
-            movie.setName("바람과 함께 사라지다");
-            movie.setPrice(10000);
 
-            em.persist(movie);
+            Member member = new Member();
+            member.setUsername("user1");
+            member.setCreatedBy("kim");
+            member.setCreatedDate(LocalDateTime.now());
+
+            em.persist(member);
 
             em.flush();
             em.clear();
-
-            Item item = em.find(Item.class , movie.getId());
-            System.out.println("findMovie = " + item);
-
             tx.commit();
         } catch(Exception e){
             tx.rollback();
---
+++ src/main/java/hellojpa/MemberProduct.java
@@ -0,0 +1,19 @@
+package hellojpa;
+
+import javax.persistence.*;
+
+@Entity
+public class MemberProduct {
+
+    @Id @GeneratedValue
+    private Long Id;
+
+    @ManyToOne
+    @JoinColumn(name="MEMBER_ID")
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name="PRODUCT_ID")
+    private Product product;
+
+}

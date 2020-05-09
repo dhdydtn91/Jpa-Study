5be0f0265a45d25bf23e1397df30d0f5b19e3cb0
dhdydtn2019 <dhdydtn2019@gmail.com>
2020-05-09 23:29:09 +0900

    지연 로딩 즉시로딩 예제

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,10 +1,6 @@
 package hellojpa;
 
-        import javax.persistence.EntityManager;
-        import javax.persistence.EntityManagerFactory;
-        import javax.persistence.EntityTransaction;
-        import javax.persistence.Persistence;
-        import java.time.LocalDateTime;
+        import javax.persistence.*;
         import java.util.List;
 
 public class JpaMain {
@@ -19,18 +15,34 @@ public class JpaMain {
         tx.begin();
 
         try {
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
+            Team teamB = new Team();
+            team.setName("teamA");
+            em.persist(teamB);
 
-            Member member = new Member();
-            member.setUsername("user1");
-            member.setCreatedBy("kim");
-            member.setCreatedDate(LocalDateTime.now());
+            Member member1 = new Member();
+            member1.setUsername("member1");
+            member1.setTeam(team);
+            em.persist(member1);
 
-            em.persist(member);
+            Member member2 = new Member();
+            member2.setUsername("member1");
+            member2.setTeam(teamB);
+            em.persist(member2);
 
             em.flush();
             em.clear();
+
+//            Member m = em.find(Member.class, member1.getId());
+            List<Member> members= em.createQuery("select m from Member m join fetch m.team", Member.class)
+                    .getResultList();
+
+            // 피치 못하게 여러개를 한번에 가져와야 할경우  fetch 조인 사용
             tx.commit();
         } catch(Exception e){
+            e.printStackTrace();
             tx.rollback();
         }finally {
             em.close();
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -15,17 +15,9 @@ public class Member extends BaseEntity{
     @Column(name = "USERNAME")
     private String username;
 
-    @ManyToOne
-    @JoinColumn(insertable = false, updatable = false)
+    @ManyToOne(fetch =  FetchType.EAGER)
+    @JoinColumn(name="TEAM_ID")
     private Team team;
-
-    @OneToOne
-    @JoinColumn(name = "LOCKER_ID")
-    private Locker locker;
-
-    @OneToMany(mappedBy = "member")
-    private List<MemberProduct> memberProducts  = new ArrayList<>();
-
 
     public Member() {
     }
@@ -44,6 +36,14 @@ public class Member extends BaseEntity{
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
     }
 
 
